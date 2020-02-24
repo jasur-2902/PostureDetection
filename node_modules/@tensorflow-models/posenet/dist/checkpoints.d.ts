556efd35df6dae507abb31e10be93b8a2d534390
Jasur Shukurov <jasur.shukurov29@gmail.com>
2020-02-24 01:53:53 -0500

    gitignore

--- node_modules/@tensorflow-models/posenet/dist/checkpoints.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function resNet50Checkpoint(stride: number, quantBytes: number): string;
-export declare function mobileNetCheckpoint(stride: number, multiplier: number, quantBytes: number): string;
--- node_modules/@tensorflow-models/posenet/dist/multi_pose/build_part_with_score_queue.d.ts
+++
@@ -1,3 +0,0 @@
-import { PartWithScore, TensorBuffer3D } from '../types';
-import { MaxHeap } from './max_heap';
-export declare function buildPartWithScoreQueue(scoreThreshold: number, localMaximumRadius: number, scores: TensorBuffer3D): MaxHeap<PartWithScore>;
